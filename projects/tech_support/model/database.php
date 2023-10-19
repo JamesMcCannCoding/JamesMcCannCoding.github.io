597ec418e75589f8b6ce7a5e2ac48fe548a14173
James McCann <mccjam16@gmail.com>
2023-10-19 13:43:09 +0800

    update

--- projects/tech_support/model/database.php
+++ projects/tech_support/model/database.php
@@ -1,15 +1,13 @@
 <?php
-    //$dsn = 'mysql:host=localhost;dbname=tech_support';
-    $server = 'sql110.infinityfree.com';
-    $username = 'if0_35254941';
-    $password = 'xovIthbe0U6J';
-    $dbname = 'if0_35254941_SportPro';
-
+    $dsn = 'mysql:host=localhost;dbname=id21419046_tech_support';
+    $username = 'id21419046_james';
+    $password = 'McCann1!';
+    
     try {
         $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         include('../errors/database_error.php');
         exit();
-    }
+    }    
 ?>
--- projects/tech_support/view/header.php
+++ projects/tech_support/view/header.php
@@ -5,22 +5,22 @@
 <head>
     <title>SportsPro Technical Support</title>
     <link rel="stylesheet" type="text/css"
-          href="/tech_support/main.css">
+          href="./main.css">
     <link rel="icon" 
-            href="/tech_support/SportsProLogo.png" type="image/x-icon">
+            href="./SportsProLogo.png" type="image/x-icon">
 </head>
 
 <!-- The body section -->
 <body>
 <header>
-    <a href="/tech_support/index.php">
-        <img src="/tech_support/SportsProLogo.png" alt="Logo" width="150" height="100" title="Home">
+    <a href="../index.php">
+        <img src="./SportsProLogo.png" alt="Logo" width="150" height="100" title="Home">
     </a>
     <h1>SportsPro Technical Support</h1>
     <p>Sports management software for the sports enthusiast</p>
     <nav>
         <ul>
-            <li><a href="/tech_support/">Home</a></li>
+            <li><a href="../index.php">Home</a></li>
         </ul>
     </nav>
 </header>
